7304a62090cdc1e7b2f6008a6d5a89c5436fee51
fagnersro <fagner.hg40@gmail.com>
2023-03-02 22:08:04 -0300

    useCallback - React Hook

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,45 +1,72 @@
+import P from 'prop-types';
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-const eventFn = () => console.log('h1-clicado');
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('filho redenrizou ');
+  return <button onClick={() => incrementButton(10)}>Comp</button>;
+});
 
-// COMPONENT EXAMPLE USEEFFECT
+Button.propTypes = {
+  incrementButton: P.func,
+};
+
 function App() {
   const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
 
-  // componentDidUpdate - executa toda vez que o componet atualiza
-  // useEffect(() => {
-  //   console.log('componentDidUpdate');
-  // });
-
-  // componentDidMount - executa 1x
-  useEffect(() => {
-    // console.log('componentDidMount');
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    // COMPONENT WILL UMOUNT - LIMPEZA
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
   }, []);
 
-  // com dependência - executa toda vez que a depedência mudar
-  useEffect(() => {
-    console.log(`C1, ${counter} C2: ${counter2}`);
-  }, [counter, counter2]);
+  console.log('pai redenrizou');
 
   return (
     <div className="App">
-      <p>Teste 1</p>
-      <h1>
-        C1: {counter} C2: {counter2}
-      </h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter2(counter2 + 1)}>+(2)</button>
+      <p>Teste 3</p>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }
+
+// COMPONENT EXAMPLE USEEFFECT
+// const eventFn = () => console.log('h1-clicado');
+// function App() {
+// const [counter, setCounter] = useState(0);
+// const [counter2, setCounter2] = useState(0);
+
+// componentDidUpdate - executa toda vez que o componet atualiza
+// useEffect(() => {
+//   console.log('componentDidUpdate');
+// });
+
+// componentDidMount - executa 1x
+// useEffect(() => {
+// console.log('componentDidMount');
+// document.querySelector('h1')?.addEventListener('click', eventFn);
+
+// COMPONENT WILL UMOUNT - LIMPEZA
+//   return () => {
+//     document.querySelector('h1')?.removeEventListener('click', eventFn);
+//   };
+// }, []);
+
+// com dependência - executa toda vez que a depedência mudar
+// useEffect(() => {
+//   console.log(`C1, ${counter} C2: ${counter2}`);
+// }, [counter, counter2]);
+
+// return (
+//   <div className="App">
+//     <p>Teste 1</p>
+//     <h1>
+//       C1: {counter} C2: {counter2}
+//     </h1>
+//     <button onClick={() => setCounter(counter + 1)}>+</button>
+//     <button onClick={() => setCounter2(counter2 + 1)}>+(2)</button>
+//   </div>
+// );
+// }
 
 //COMPONENT EXAMPLE USESTATE
 // function App() {
