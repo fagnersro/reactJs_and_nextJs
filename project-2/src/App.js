6bb5fedb3c0bbc2dcd1373876ecdc2b82c278a53
fagnersro <fagner.hg40@gmail.com>
2023-03-03 18:17:37 -0300

    useMemo - React Hook

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,33 +1,90 @@
 import P from 'prop-types';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
-import React, { useState, useEffect, useCallback } from 'react';
 
-const Button = React.memo(function Button({ incrementButton }) {
-  console.log('filho redenrizou ');
-  return <button onClick={() => incrementButton(10)}>Comp</button>;
-});
+const Post = ({ post }) => {
+  console.log('filho redenrizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
 
-Button.propTypes = {
-  incrementButton: P.func,
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
 
-  const incrementCounter = useCallback((num) => {
-    setCounter((c) => c + num);
+  console.log('pai renderizou');
+  // component did mount
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
   }, []);
-
-  console.log('pai redenrizou');
 
   return (
     <div className="App">
-      <p>Teste 3</p>
-      <h1>C1: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })
+        );
+      }, [posts])}
+      {posts.length <= 0 && <p>Ainda não exitem posts.</p>}
     </div>
   );
 }
+
+//  EXEMPLO DE CALLBACK
+// import React, { useState, useEffect, useCallback } from 'react';
+
+// const Button = React.memo(function Button({ incrementButton }) {
+//   console.log('filho redenrizou ');
+//   return <button onClick={() => incrementButton(10)}>Comp</button>;
+// });
+
+// Button.propTypes = {
+//   incrementButton: P.func,
+// };
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+
+//   const incrementCounter = useCallback((num) => {
+//     setCounter((c) => c + num);
+//   }, []);
+
+//   console.log('pai redenrizou');
+
+//   return (
+//     <div className="App">
+//       <p>Teste 3</p>
+//       <h1>C1: {counter}</h1>
+//       <Button incrementButton={incrementCounter} />
+//     </div>
+//   );
+// }
 
 // COMPONENT EXAMPLE USEEFFECT
 // const eventFn = () => console.log('h1-clicado');
