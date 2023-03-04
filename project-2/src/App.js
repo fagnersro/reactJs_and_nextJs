22badc13551c3bf278c81d5672a91f8bd82cd1b0
fagnersro <fagner.hg40@gmail.com>
2023-03-04 00:10:10 -0300

    useContext - Reat Hook

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,78 +1,91 @@
-import P from 'prop-types';
-import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
-
-const Post = ({ post, handleClick }) => {
-  console.log('filho redenrizou');
-  return (
-    <div key={post.id} className="post">
-      <h1 onClick={() => handleClick(post.title)}>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
-  );
-};
-
-Post.propTypes = {
-  post: P.shape({
-    id: P.number,
-    title: P.string,
-    body: P.string,
-  }),
-  handleClick: P.func,
-};
+import { Div } from './components/Div';
+import { AppContext } from './components/contexts/AppContext';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [value, setValue] = useState('');
-  const input = useRef(null);
-  const contador = useRef(0);
-
-  console.log('pai renderizou');
-  // component did mount
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((r) => r.json())
-      .then((r) => setPosts(r));
-  }, []);
-
-  useEffect(() => {
-    input.current.focus();
-    console.log(input.current);
-  }, [value]);
-
-  useEffect(() => {
-    contador.current++;
-  });
-
-  const handleClick = (value) => {
-    setValue(value);
-  };
-
   return (
-    <div className="App">
-      <h1>Renderizou: {contador.current}</h1>
-
-      <p>
-        <input
-          ref={input}
-          type="search"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </p>
-
-      {useMemo(() => {
-        return (
-          posts.length > 0 &&
-          posts.map((post) => {
-            return <Post key={post.id} post={post} handleClick={handleClick} />;
-          })
-        );
-      }, [posts])}
-      {posts.length <= 0 && <p>Ainda não exitem posts.</p>}
-    </div>
+    <AppContext>
+      <Div />
+    </AppContext>
   );
 }
+
+//EXEMPLO DE USEREF
+// import P from 'prop-types';
+// import { useEffect, useMemo, useRef, useState } from 'react';
+// import './App.css';
+
+// const Post = ({ post, handleClick }) => {
+//   console.log('filho redenrizou');
+//   return (
+//     <div key={post.id} className="post">
+//       <h1 onClick={() => handleClick(post.title)}>{post.title}</h1>
+//       <p>{post.body}</p>
+//     </div>
+//   );
+// };
+
+// Post.propTypes = {
+//   post: P.shape({
+//     id: P.number,
+//     title: P.string,
+//     body: P.string,
+//   }),
+//   handleClick: P.func,
+// };
+
+// function App() {
+//   const [posts, setPosts] = useState([]);
+//   const [value, setValue] = useState('');
+//   const input = useRef(null);
+//   const contador = useRef(0);
+
+//   console.log('pai renderizou');
+//   // component did mount
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//       .then((r) => r.json())
+//       .then((r) => setPosts(r));
+//   }, []);
+
+//   useEffect(() => {
+//     input.current.focus();
+//     console.log(input.current);
+//   }, [value]);
+
+//   useEffect(() => {
+//     contador.current++;
+//   });
+
+//   const handleClick = (value) => {
+//     setValue(value);
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Renderizou: {contador.current}</h1>
+
+//       <p>
+//         <input
+//           ref={input}
+//           type="search"
+//           value={value}
+//           onChange={(e) => setValue(e.target.value)}
+//         />
+//       </p>
+
+//       {useMemo(() => {
+//         return (
+//           posts.length > 0 &&
+//           posts.map((post) => {
+//             return <Post key={post.id} post={post} handleClick={handleClick} />;
+//           })
+//         );
+//       }, [posts])}
+//       {posts.length <= 0 && <p>Ainda não exitem posts.</p>}
+//     </div>
+//   );
+// }
 
 // EXEMPLO USEMEMO
 // import P from 'prop-types';
